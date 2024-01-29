251f4238ebe1acb7226d7c6fe9cb30285c4624f5
federico <federico@pop-os.localdomain>
2024-01-29 10:48:48 +0100

    Resolgin conflicts with main

--- web-app/src/main/java/it/unipi/dsmt/utility/Params.java
+++ web-app/src/main/java/it/unipi/dsmt/utility/Params.java
@@ -4,15 +4,17 @@ public class Params {
     public static final String TOPIC_CARS = "cars-data";
     public static final String CARS_DATA_GROUP = "0";
 
-    public static final String TOPIC_TRIPS = "trip-reports";
-    public static final String TRIPS_DATA_GROUP = "2";
-
     public static final String TOPIC_FRAUDS = "frauds";
     public static final String FRAUDS_GROUP = "1";
+
+        public static final String TOPIC_TRIPS = "trip-reports";
+    public static final String TRIPS_DATA_GROUP = "2";
+
     public static final String KAFKA_ENDPOINTS = "kafka-broker-1:9093,kafka-broker-2:9095";
     public static final String KEY_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
     public static final String VALUE_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
     public static final String AUTO_OFFSET_RESET = "latest";
+
     public static final int THREADS = 2;
     public static final int PARTITIONS_PER_TOPIC = 2;
     public static final int POLL_DURATION = 100;
